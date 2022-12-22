6e629072edd5109af9d5753f244170cbec06ecf0
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-22 17:59:35 +0100

    Update TargetFramework

--- src/Generators/PocoSchemaGenerator/Oxx.Backend.Generators.PocoSchema.Core/Models/FileInformation.cs
+++ src/Generators/PocoSchemaGenerator/Oxx.Backend.Generators.PocoSchema.Core/Models/FileInformation.cs
@@ -1,13 +1,6 @@
 namespace Oxx.Backend.Generators.PocoSchema.Core.Models;
 
-public readonly struct FileInformation
+public readonly record struct FileInformation(FileName Name, FileContent Content)
 {
-	public static readonly FileInformation None = new()
-	{
-		Content = FileContent.None,
-		Name = FileName.None,
-	};
-
-	public required FileName Name { get; init; }
-	public required FileContent Content { get; init; }
+	public static readonly FileInformation None = new(FileName.None, FileContent.None);
 }
