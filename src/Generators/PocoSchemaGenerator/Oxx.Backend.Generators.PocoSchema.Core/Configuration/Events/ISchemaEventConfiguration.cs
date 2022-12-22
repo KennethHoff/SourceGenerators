d8511a982862a30742535020fe36dc48fdb60b30
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-22 17:54:42 +0100

    Remove access modifier from interface

--- src/Generators/PocoSchemaGenerator/Oxx.Backend.Generators.PocoSchema.Core/Configuration/Events/ISchemaEventConfiguration.cs
+++ src/Generators/PocoSchemaGenerator/Oxx.Backend.Generators.PocoSchema.Core/Configuration/Events/ISchemaEventConfiguration.cs
@@ -2,8 +2,8 @@ namespace Oxx.Backend.Generators.PocoSchema.Core.Configuration.Events;
 
 public interface ISchemaEventConfiguration
 {
-	public EventHandler<FileCreatingEventArgs>? FileCreating { get; set; }
-	public EventHandler<FileCreatedEventArgs>? FileCreated { get; set; }
-	public EventHandler<FilesCreatingEventArgs>? FilesCreating { get; set; }
-	public EventHandler<FilesCreatedEventArgs>? FilesCreated { get; set; }
+	EventHandler<FileCreatingEventArgs>? FileCreating { get; set; }
+	EventHandler<FileCreatedEventArgs>? FileCreated { get; set; }
+	EventHandler<FilesCreatingEventArgs>? FilesCreating { get; set; }
+	EventHandler<FilesCreatedEventArgs>? FilesCreated { get; set; }
 }
