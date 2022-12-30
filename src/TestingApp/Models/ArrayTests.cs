59eb0bc5622460b4af6bc2643f4fef1bf14aceb6
Kenneth Hoff <kenneth.98@hotmail.com>
2022-12-30 20:43:02 +0100

    Fix a bug where arrays imported nothing from nothing

---
+++ src/TestingApp/Models/ArrayTests.cs
@@ -0,0 +1,13 @@
+﻿using Oxx.Backend.Generators.PocoSchema.Core.Attributes;
+
+namespace TestingApp.Models;
+
+[SchemaObject]
+internal sealed class ArrayTests
+{
+	public required int[] IntArray { get; set; }
+	public required int[]? NullableIntArray { get; set; }
+
+	public required string[] StringArray { get; set; }
+	public required string[]? NullableStringArray { get; set; }
+}
